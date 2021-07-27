c01542e40951a76ad496fad797c123102353a8a1
doosanpark <erid3232@naver.com>
2021-07-28 00:00:45 +0900

    포트번호 3001 request 시도하다가 실패

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Param, Res} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(':userId')
+  findOne(@Param('userId') id: string, @Res() res): string {
+    return res.status(200).send({id, userName: '이정주', accountNum:123});
   }
 }
