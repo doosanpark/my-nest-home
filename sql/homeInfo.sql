8f797d37236c21c04304606ebc7d8ab79c3ee2a5
SYKim <syk0707@naver.com>
2021-07-30 23:17:34 +0900

    [search] SQL문 추가

---
+++ sql/homeInfo.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `homeinfo` (
+   `id` bigint NOT NULL,
+   `subject` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+   `floor` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+   `price` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+   `area` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+   `summary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+   `content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci,
+   `linkUrl` varchar(255) DEFAULT NULL,
+   `thumbnailImagePath` varchar(255) DEFAULT NULL,
+   `creator` varchar(30) DEFAULT NULL,
+   `modifier` varchar(30) DEFAULT NULL,
+   `createdDate` datetime DEFAULT NULL,
+   `modifyDate` datetime DEFAULT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+
+ INSERT INTO `my_home`.`homeinfo` (`id`, `subject`, `floor`, `price`, `area`, `summary`, `content`, `linkUrl`, `thumbnailImagePath`, `creator`, `createdDate`) 
+VALUES ('1', '마포자이 101동', '11/25층', '19억', '149/107㎡', '마포역 공덕역 도보거리', '', '', '', 'SYKim', '2021-07-30');
