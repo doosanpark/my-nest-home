021ea6239ac6d6a31a749ea9c8952c11808540ae
SYKim <syk0707@naver.com>
2021-08-20 00:15:11 +0900

    [search] Insert 추가

--- src/controller/search.controller.ts
+++ src/controller/search.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Param, Body, Res } from '@nestjs/common';
 import { AppService } from '../app.service';
 import { HomeInfoEntity } from 'src/entity/homeinfo.entity';
-import { createQueryBuilder } from 'typeorm';
+import { getRepository, createQueryBuilder } from 'typeorm';
 
 @Controller('api/search')
 export class SearchController {
@@ -12,13 +12,32 @@ export class SearchController {
     const retVal = {
       searchKeyword: keyword,
       searchList: [],
-    }; 
+    };
 
     try {
       const result = await createQueryBuilder('homeInfo')
         .where('subject like :keyword', { keyword: `%${keyword}%` })
         .getMany();
       retVal.searchList = result;
+    } catch (e) {
+      console.log(e);
+    }
+
+    return res.status(200).send(retVal);
+  }
+
+  @Post('insert')
+  async insert(
+    @Body('homeInfo') homeInfo: HomeInfoEntity,
+    @Res() res,
+  ): Promise<string> {
+    const retVal = {
+      success: false,
+    };
+    try {
+      const homeRepository = getRepository(HomeInfoEntity);
+      homeRepository.save(homeInfo);
+      retVal.success = true;
     } catch (e) {
       console.log(e);
     }
