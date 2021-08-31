6bdb9cc7b6ebb307f6bfdf5e15b742d12d0b1f8c
doosanpark <erid3232@naver.com>
2021-09-01 01:12:21 +0900

    no message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { SearchController } from './controller/search.controller';
+import { AccountController } from './controller/account.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forRoot()],
-  controllers: [AppController, SearchController],
+  controllers: [AppController, SearchController, AccountController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
+ 
