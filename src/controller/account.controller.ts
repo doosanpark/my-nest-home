61e625b1a448711d1d297815f13a8f943e771605
doosanpark <erid3232@naver.com>
2021-09-02 00:18:26 +0900

    no message

--- src/controller/account.controller.ts
+++ src/controller/account.controller.ts
@@ -2,13 +2,12 @@ import { Controller, Get, Post, Param, Body, Res } from '@nestjs/common';
 import { AppService } from '../app.service';
 import { HomeInfoEntity } from 'src/entity/homeinfo.entity';
 import { createQueryBuilder } from 'typeorm';
-
 @Controller('api/account')
 export class AccountController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Post('login')
-  async list(@Body('telInfo') telInfo: string, @Body('passInfo') passInfo: string,  @Res() res): Promise<string> {
+  async list(@Body('telInfo') telInfo: string, @Body('passInfo') passInfo: string, @Res() res): Promise<string> {
     const retVal = {
       telInfo: telInfo,
       passInfo: passInfo,
