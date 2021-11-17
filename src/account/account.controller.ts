a650db8430ca8c6a33351205469a9fd8a08add36
doosanpark <erid3232@naver.com>
2021-11-17 22:10:36 +0900

    로그인  select 쿼리 문제 해결

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -13,11 +13,6 @@ export class AccountController {
 
   @Post('check')
   async checkData(@Body('info') info: string, @Res() res): Promise<string> {
-
-
-
-    console.log("info", info);
-    console.log("res", res);
     return res.status(200).send();
   }
 
@@ -29,20 +24,15 @@ export class AccountController {
       searchList: [],
     };
 
-    console.log("res", res);
-    console.log("retVal", retVal);
-    console.log("passInfo", passInfo);
-    console.log("telInfo", telInfo);
-    console.log("log", this.accountService.getAccount);
-
-    // try {
-    //   const result = await createQueryBuilder('homeInfo')
-    //     .where('subject like :keyword', { keyword: `%${keyword}%` })
-    //     .getMany();
-    //   retVal.searchList = result;
-    // } catch (e) {
-    //   console.log(e);
-    // }
+    try {
+      const result = await createQueryBuilder('account')
+        .where("phone = :phone", { phone: retVal.telInfo })
+        .andWhere("pass = :pass", { pass: retVal.passInfo })
+        .getMany();
+      retVal.searchList = result;
+    } catch (e) {
+      console.log(e);
+    }
 
     return res.status(200).send(retVal);
   }
